db519b2b7adf315bdb5f4763e851a6a73b105923
Valeriya Stange <valeriya.stange@gmail.com>
2025-02-12 13:09:52 +0100

    Templates script with JSDoc in English

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,6 +1,20 @@
+/**
+ * Generates an HTML template for rendering a Pokémon card.
+ * Each Pokémon card has a front and back side, displaying its details, image, and abilities.
+ *
+ * @param {number} i - The index of the Pokémon in `currentPokemonDataList`.
+ * @param {Object} onePokemon - The Pokémon object containing its details.
+ * @param {string} onePokemon.name - The name of the Pokémon.
+ * @param {number} onePokemon.id - The ID of the Pokémon.
+ * @param {string} onePokemon.imgUrl - The URL of the Pokémon's main image.
+ * @param {string} onePokemon.gifUrl - The URL of the Pokémon's animated GIF.
+ * @param {Array} onePokemon.types - An array of type objects associated with the Pokémon.
+ * @returns {string} - An HTML template string for rendering a Pokémon card.
+ */
 function generateTemplatesForRenderPokemons(i, onePokemon) {
     const formattedName = onePokemon.name.charAt(0).toUpperCase() + onePokemon.name.slice(1);
     let color = colours[onePokemon.types[0].type.name];
+
     return ` 
         <div class="pokemonCard flipped">
             <div class="cardsInside">
@@ -19,13 +33,22 @@ function generateTemplatesForRenderPokemons(i, onePokemon) {
                 </div>
             </div>
         </div>
-    `
+    `;
 }
 
+/**
+ * Generates an HTML template for rendering the Pokémon overlay card.
+ * The overlay displays detailed information about a selected Pokémon, including its name, image, 
+ * and options to view additional details (About & Stats).
+ *
+ * @param {number} i - The index of the Pokémon in `currentPokemonDataList`.
+ * @returns {string} - An HTML template string for rendering the Pokémon overlay card.
+ */
 function generateTemplatesForOverlay(i) {
     let onePokemon = currentPokemonDataList[i];
     let color = colours[onePokemon.types[0].type.name];
     const formattedName = onePokemon.name.charAt(0).toUpperCase() + onePokemon.name.slice(1);
+    
     return `
     <div class="overlayCard" style="background:${color} !important" onclick="stopPropation(event)">
         <img class="pokemonImgOverlayCard" src="${onePokemon.imgUrl}" alt="img. Pokemon">
@@ -44,9 +67,16 @@ function generateTemplatesForOverlay(i) {
             <button onclick="nextBtnInOverlay()" id="next"></button>
         </div>
     </div>
-    `
+    `;
 }
 
+/**
+ * Generates an HTML template for displaying the Pokémon's type icons.
+ * Iterates through the Pokémon's types and creates an image element for each type.
+ *
+ * @param {Object} onePokemon - The Pokémon object containing type information.
+ * @returns {string} - An HTML string containing image elements for each type.
+ */
 function generateTemplatesTypes(onePokemon) {
     let template = ``;
     for (let index = 0; index < onePokemon.types.length; index++) {
@@ -56,30 +86,45 @@ function generateTemplatesTypes(onePokemon) {
     return template;
 }
 
+/**
+ * Generates an HTML template displaying details about a Pokémon.
+ * The template includes height, weight, abilities, and base experience.
+ *
+ * @param {number} i - The index of the Pokémon in the current list.
+ * @returns {string} - An HTML string representing a table with the Pokémon's details.
+ */
 function generateTemplatesAbout(i) {
     let onePokemon = currentPokemonDataList[i];
     let template = ``;
     for (let i = 0; i < onePokemon.abilities.length; i++) {
-        template += `<p>${onePokemon.abilities[i].ability.name}</p>`
+        template += `<p>${onePokemon.abilities[i].ability.name}</p>`;
     }
-    let templateAbout =`
+    
+    let templateAbout = `
     <table>
         <tr><td><b>Height:</b></td><td>${onePokemon.height}cm</td></tr>
         <tr><td><b>Weight:</b></td><td>${onePokemon.weight}kg</td></tr>
         <tr><td><b>Abilities:</b></td><td>${template}</td></tr>
-        <tr><td><b>BaseExperience:</b></td><td>${onePokemon.baseExperience}xp</td></tr>
+        <tr><td><b>Base Experience:</b></td><td>${onePokemon.baseExperience}xp</td></tr>
     </table>
-    `
+    `;
+    
     return templateAbout;
 }
 
+/**
+ * Generates an HTML template containing a canvas element 
+ * for displaying the Pokémon's stats in a chart.
+ *
+ * @param {number} i - The index of the Pokémon in the current list.
+ * @returns {string} - An HTML string with a container for the stats chart.
+ */
 function generateTemplatesStats(i) {
-    let template =`
+    let template = `
     <div class="containerCanvasStats">
-        <canvas id="myChart" width="450" height="300" ></canvas>
+        <canvas id="myChart" width="450" height="300"></canvas>
     </div>
     `;
 
     return template;
 }
-

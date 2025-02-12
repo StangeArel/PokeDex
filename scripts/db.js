a9020029cd0211d535e277dc0a520293457351d7
Valeriya Stange <valeriya.stange@gmail.com>
2025-02-12 12:45:46 +0100

    DB script with JSDoc in English

--- scripts/db.js
+++ scripts/db.js
@@ -1,11 +1,72 @@
+/**
+ * Base URL for the Pokémon API.
+ * Used to fetch Pokémon data.
+ * @constant {string}
+ */
 const pokeApiUrl = "https://pokeapi.co/api/v2/pokemon";
+
+/**
+ * The number of Pokémon to fetch per API request.
+ * @type {number}
+ */
 let pokeApiLimit = 30;
+
+/**
+ * The offset for paginated API requests.
+ * Determines where the next batch of Pokémon data starts.
+ * @type {number}
+ */
 let pokeApiOffset = 0;
+
+/**
+ * Reference to the main HTML element where Pokémon data will be displayed.
+ * @constant {HTMLElement}
+ */
 const pokemonList = document.getElementById("main");
+
+/**
+ * Array to store fetched Pokémon data.
+ * @type {Array}
+ */
 let pokemonDataList = [];
+
+/**
+ * Tracks the current Pokémon index for navigation.
+ * @type {number}
+ */
 let currentIndex;
+
+/**
+ * Stores the currently displayed Pokémon data.
+ * Used for filtering or pagination.
+ * @type {Array}
+ */
 let currentPokemonDataList = [];
 
+/**
+ * An object that maps Pokémon types to their corresponding RGBA color values.
+ * These colors are used for visual representation of different Pokémon types.
+ * 
+ * @constant {Object.<string, string>}
+ * @property {string} normal - Color for normal-type Pokémon.
+ * @property {string} fire - Color for fire-type Pokémon.
+ * @property {string} water - Color for water-type Pokémon.
+ * @property {string} electric - Color for electric-type Pokémon.
+ * @property {string} grass - Color for grass-type Pokémon.
+ * @property {string} ice - Color for ice-type Pokémon.
+ * @property {string} fighting - Color for fighting-type Pokémon.
+ * @property {string} poison - Color for poison-type Pokémon.
+ * @property {string} ground - Color for ground-type Pokémon.
+ * @property {string} flying - Color for flying-type Pokémon.
+ * @property {string} psychic - Color for psychic-type Pokémon.
+ * @property {string} bug - Color for bug-type Pokémon.
+ * @property {string} rock - Color for rock-type Pokémon.
+ * @property {string} ghost - Color for ghost-type Pokémon.
+ * @property {string} dragon - Color for dragon-type Pokémon.
+ * @property {string} dark - Color for dark-type Pokémon.
+ * @property {string} steel - Color for steel-type Pokémon.
+ * @property {string} fairy - Color for fairy-type Pokémon.
+ */
 const colours = {
     normal: 'rgba(168, 167, 122, 0.6)',
     fire: 'rgba(238, 129, 48, 0.6)',
